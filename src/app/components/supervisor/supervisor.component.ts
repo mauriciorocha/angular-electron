907c0105e41735a0c4f246443fbf02b0b8580c2c
Ana Flávia Carvalho <di48150>
2017-08-11 18:02:43 -0300

    mostrando os grupos

--- src/app/components/supervisor/supervisor.component.ts
+++ src/app/components/supervisor/supervisor.component.ts
@@ -4,7 +4,8 @@
 import { Component, EventEmitter } from "@angular/core"
 import { MaterializeDirective, MaterializeAction } from "angular2-materialize";
 
-import {ReversePipe} from 'ngx-pipes/src/app/pipes/array/reverse';
+import {GroupByPipe} from 'ngx-pipes/src/app/pipes/array/group-by';
+
 
 import * as supervisord from 'supervisord';
 
@@ -14,10 +15,12 @@ import { config } from '../../config/config';
     selector: 'tabs-supervisor',
     templateUrl: './supervisor.component.html',
     styleUrls: ['./supervisor.component.scss'],
+    providers: [GroupByPipe]
 })
 export class SupervisorComponent {
   clientSupervisord: any;
   infoServers: any = [];
+  arr: any = [];
   params = [
     {
       onOpen: (el) => {
@@ -29,7 +32,8 @@ export class SupervisorComponent {
     }
   ];
 
-  constructor(private reversePipe: ReversePipe) {
+  constructor(private _groupByPipe: GroupByPipe
+              ) {
     // this.pidAtivos = this.books.filter(book => book.store_id === this.store.id);
     for (let host of config.supervisor.hosts) {
       //this.serverHosts.push(host.nameHost);
@@ -37,18 +41,32 @@ export class SupervisorComponent {
       let that = this;
       
       this.clientSupervisord.getAllProcessInfo(function(err, result) {
+        let grouped = that._groupByPipe.transform(result, 'group');
+        console.log(grouped);
+        let arrRetorno = [];
+        let arr = [];
+        Object.keys(grouped).forEach( chave => {
+
+            
+            arr.push(result.filter(obj => obj.group === chave));
+            // let arr = [];
+            // arrRetorno[chave] = grouped[chave];
+            //arrRetorno.push(arr);
+        });
+        console.log(arr);
           //Here we may get cookie received from server if we know its name
         that.infoServers.push(
           {
             "pidAtivos": result.filter(obj => obj.statename === "RUNNING").length,
             "pidInativos": result.filter(obj => obj.statename !== "RUNNING").length,
             "nameHost": host.nameHost, 
-            "result": result
+            "result": arrRetorno
           }
         );
+        
       });
-
-      console.log(this.infoServers);
+console.log(this.infoServers);
+      
     }
   }
 }
