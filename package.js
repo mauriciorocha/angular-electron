db0df3f71772a3d13871849de732a24828eb92b9
Mauricio S. Rocha <mauricio.rocha@brscan.com.br>
2017-08-18 16:56:16 -0300

    geração de build

--- package.js
+++ package.js
@@ -1,48 +1,91 @@
 "use strict";
 
 var packager = require('electron-packager');
+var colors = require('colors');
+var fs = require('fs');
 const pkg = require('./package.json');
 const argv = require('minimist')(process.argv.slice(2));
 const devDeps = Object.keys(pkg.devDependencies);
-
-const appName = argv.name || pkg.productName;
+const appName = argv.name || pkg.productName || pkg.name;
 const shouldUseAsar = argv.asar || false;
 const shouldBuildAll = argv.all || false;
-const arch = argv.arch || 'all';
-const platform = argv.platform || 'darwin';
+const arch = argv.arch || 'x64';
+// const arch = argv.arch || 'all';
+// const platform = argv.platform || 'win32';
+const platform = argv.platform || 'linux';
+// const platform = argv.platform || 'darwin';
+var ncp = require('ncp').ncp;
 
 const DEFAULT_OPTS = {
     dir: './src/app',
     name: appName,
     asar: shouldUseAsar,
-    ignore: [
-    ].concat(devDeps.map(name => `/node_modules/${name}($|/)`))
-};
+    ignore: [].concat(devDeps.map(name => `/node_modules/${name}($|/)`))
+}
 
-const icon = './src/app/dist/assets/app-icon';
+// const icon = './src/app/dist/assets/app-icon';
+const icon = './dist/assets/docker-sm.png';
 
 if (icon) {
     DEFAULT_OPTS.icon = icon;
 }
 
-pack(platform, arch, function done(err, appPath) {
-    console.log(err);
+pack(platform, arch, function(err, appPath) {
+    // console.log(DEFAULT_OPTS);
+    if (err) {
+        console.error(("Gerando build... " + err).red);
+    } else {
+        console.log("Build gerada com sucesso!".green);
+        console.log(("Gerado em: ").green + (__dirname + '/' + appPath[0]).underline.green);
+        console.log("Copiando imagens e fonts...".magenta.italic);
+
+        const mainjs = __dirname + '/main.js';
+        const indexhtml = __dirname + '/src/app/index.html';
+        const output = __dirname + `/release/${platform}-${arch}/${appName}-${platform}-${arch}`;
+
+        console.log(`Copiando ${mainjs} para ${output}/main.js`.magenta);
+        console.log(`Copiando ${indexhtml} para ${output}/resources/app/index.html`.magenta);
+
+        fs.createReadStream(mainjs).pipe(fs.createWriteStream(output + '/resources/app/main.js'));
+        fs.createReadStream(indexhtml).pipe(fs.createWriteStream(output + '/resources/app/index.html'));
+
+        const source = __dirname + '/src/assets';
+        const destination = output + '/resources/assets';
+
+        ncp(source, destination, function(err) {
+            if (err) {
+                return console.error(('Erro ao copiar as imagens. ' + err).red);
+            }
+            console.log('Imagens copiadas com sucesso!'.green);
+        });
+
+        // const sourceFonts = __dirname + '/node_modules/materialize-css/dist/fonts';
+        // const destinationFonts = output + '/resources/fonts';
+        // ncp(sourceFonts, destinationFonts, function (err) {
+        //     if (err) {
+        //         return console.error(('ERROR COPYING FONTS! ' + err).red);
+        //     }
+        //     console.log('FONTS COPIED SUCCESSFULLY'.green);
+        // });
+        const sourceFonts = __dirname + '/src/fonts';
+        const destinationFonts = output + '/resources/fonts';
+        ncp(sourceFonts, destinationFonts, function(err) {
+            if (err) {
+                return console.error(('Erro ao copiar fonts.' + err).red);
+            }
+            console.log('Fonts copiadas com sucesso!'.green);
+        });
+    }
 });
 
 function pack(plat, arch, cb) {
     // there is no darwin ia32 electron
-    if (plat === 'darwin' && arch === 'ia32') return;
+    if (plat === 'darwin' && arch === 'ia32') {
+        return;
+    }
 
     const iconObj = {
-        icon: DEFAULT_OPTS.icon + (() => {
-            let extension = '.png';
-            if (plat === 'darwin') {
-                extension = '.icns';
-            } else if (plat === 'win32') {
-                extension = '.ico';
-            }
-            return extension;
-        })()
+        icon: DEFAULT_OPTS.icon
     };
 
     const opts = Object.assign({}, DEFAULT_OPTS, iconObj, {
@@ -50,9 +93,13 @@ function pack(plat, arch, cb) {
         arch,
         prune: true,
         all: shouldBuildAll,
-        'app-version': pkg.version || DEFAULT_OPTS.version,
+        overwrite: true,
+        appVersion: pkg.version || DEFAULT_OPTS.version,
         out: `release/${plat}-${arch}`
     });
+
+    console.log('\x1Bc');
+    console.log('Gerando build do app '.magenta.italic + appName.bold.magenta + '...'.magenta);
 
     packager(opts, cb);
 }
